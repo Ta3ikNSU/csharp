e836484b94aa780bf2ec35ce88d9185d067246f2
Dmitriy Korolev <58227907+Ta3ikNSU@users.noreply.github.com>
2022-10-16 00:54:20 +0700

    [lab3] - add test

---
+++ lab3/FriendTests.cs
@@ -0,0 +1,20 @@
+﻿using lab2.model;
+using lab2.model.Interfaces;
+
+namespace lab3;
+
+public class FriendTests
+{
+    [Test]
+    public void UniqueContendersName()
+    {
+        IContenderGenerator contenderGenerator = new ContenderGenerator();
+        var friend = new Friend(contenderGenerator);
+        var contenders = contenderGenerator.GetContenders();
+        var firstContender = contenders[0];
+        var lastContender = contenders[^1];
+        var firstRating = friend.GetRank(firstContender);
+        var lastRating = friend.GetRank(lastContender);
+        Assert.That(friend.CompareContenders(firstContender, lastContender), Is.EqualTo(firstRating > lastRating));
+    }
+}
---
+++ lab3/PrincessTests.cs
@@ -0,0 +1,35 @@
+﻿using lab2.model;
+using lab2.model.Interfaces;
+
+namespace lab3;
+
+public class PrincessTests
+{
+    [Test]
+    public void WhatTestHere()
+    {
+        IContenderGenerator contenderGenerator = new ContenderGenerator();
+        IFriend friend = new Friend(contenderGenerator);
+        IStrategy strategy = new SkipStrategy(friend, 1);
+        IPrincess princess = new Princess(strategy);
+        var contenders = contenderGenerator.GetContenders();
+        IContender bestOption = null;
+        foreach (var contender in contenders)
+        {
+            if (friend.GetRank(contender) < 55) continue;
+            bestOption = contender;
+            Assert.That(princess.SelectHusband(bestOption), Is.False);
+            break;
+        }
+        IContender secondContender;
+        foreach (var contender in contenders)
+        {
+            if (friend.GetRank(contender) >= 55) continue;
+            secondContender = contender;
+            Assert.That(princess.SelectHusband(secondContender), Is.False);
+            break;
+        }
+
+        if (bestOption != null) Assert.That(princess.GetBestContender()!, Is.EqualTo(bestOption));
+    }
+}
