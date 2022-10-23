ae58dd1676243fc71a8d70cee3f30b0e0901f7b4
Dmitriy Korolev <58227907+Ta3ikNSU@users.noreply.github.com>
2022-10-23 22:23:42 +0700

    lab4

---
+++ lab4/AttemptContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace lab4;
+
+public class AttemptContext : DbContext
+{
+    protected AttemptContext()
+        : base("DbConnection")
+    {
+    }
+
+    public DbSet<ChoiceAttempt> Attempts { get; set; }
+}
--- lab4/ChoiceAttempt.cs
+++ lab4/ChoiceAttempt.cs
@@ -4,16 +4,19 @@ using lab2.model.Interfaces;
 
 namespace lab4;
 
+[Table("ChoiceAttempt")]
 public class ChoiceAttempt
 {
-    
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
     public Guid Id { get; set; }
-    
-    public int NumberAttempt{ get; set; }
 
-    public Dictionary<IContender, int> ContendersRating { get; set;}
-    
-    public Queue<IContender> Contenders { get; set;}
+    [Required] [Column("number_attempt")] public int NumberAttempt { get; set; }
+
+    [Required]
+    [Column("contenders_rating")]
+    public Dictionary<IContender, int> ContendersRating { get; set; }
+
+    [Required] [Column("contenders")] public Queue<IContender> Contenders { get; set; }
 }
