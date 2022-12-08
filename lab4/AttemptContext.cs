04ae2ccea1ebd85168c5d9c38e38cda4a20aea48
Dmitriy Korolev <58227907+Ta3ikNSU@users.noreply.github.com>
2022-12-09 02:09:16 +0700

    add test anvironment

--- lab4/AttemptContext.cs
+++ lab4/AttemptContext.cs
@@ -11,6 +11,22 @@ public sealed class AttemptContext : DbContext
         Database.EnsureCreated();
     }
     
+    public AttemptContext(DbContextOptions<AttemptContext> options) : base(options)
+    {
+    }
+    
+    // protected override void OnModelCreating(ModelBuilder modelBuilder)
+    // {
+    //     modelBuilder.Entity<ChoiceAttemptDao>(builder =>
+    //     {
+    //         builder.ToTable("Attempts");
+    //         builder.HasKey(dao => dao.Id);
+    //         builder.Property(dao => dao.Number).IsRequired().HasComment("Номер, которым претендент отправится к принцессе");
+    //         builder.Property(dao => dao.Rating).IsRequired().HasComment("Рейтинг претендента");
+    //         builder.Property(dao => dao.Name).IsRequired().HasComment("Имя претендента");
+    //         builder.Property(dao => dao.Patronymic).IsRequired().HasComment("Фамилия претендента");
+    //     });
+    // }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseNpgsql("Host=localhost:5432;Database=csharpdb;Username=postgres;Password=1234");
---
+++ lab4/DBTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace lab4;
+
+public class DBTest
+{
+    private static DbContextOptions<AttemptContext> DbContextOptions =
+        (DbContextOptions<AttemptContext>)new DbContextOptionsBuilder()
+            .UseInMemoryDatabase(databaseName: "attempts_test")
+            .Options;
+    
+    private AttemptContext _context;
+    
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        _context = new AttemptContext(DbContextOptions);
+        _context.Database.EnsureCreated();
+    }
+    
+    [OneTimeTearDown]
+    public void Cleanup()
+    {
+        _context.Database.EnsureDeleted();
+    }
+}
